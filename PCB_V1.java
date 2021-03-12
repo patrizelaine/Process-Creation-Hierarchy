627a85dfbd4d3e519d484f4791360594878ddbb2
AlexaTTang <46989357+AlexaTTang@users.noreply.github.com>
2021-03-12 13:48:43 -0800

    PCB_V1
    PCB implementation with linked lists

---
+++ PCB_V1.java
@@ -0,0 +1,26 @@
+package Process-Creation_Hierarchy;
+import java.util.LinkedList;
+
+public class PCB
+{
+    private int index_parent;
+
+    //any processes created by the parent process are
+    //held in this linked list of child processes
+    LinkedList<PCB> children = new LinkedList<>();
+
+    PCB(int p_i)
+    {
+        p_i = index_parent;
+    }
+
+    public void addChild(PCB child)
+    {
+        children.add(child);
+    }
+    
+    public LinkedList<PCB> getChildren()
+    {
+        return children;
+    }
+}
