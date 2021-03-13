606730e9e457b4f3e4e496646c7321c6229dde47
TeraByte1 <teraesver@gmail.com>
2021-03-13 00:54:09 -0800

    update pcb version 1

--- PCB_V1.java
+++ PCB_V1.java
@@ -1,35 +1,44 @@
 /* CS 4310
-*  Group Assignment #1
-*  
-*  Group Members:
-*  Teresita Leonor S. Esver 
-*  Patriz Elaine M. Daroy
-*  Alexa Tang
-*/
+ *  Group Assignment #1
+ *
+ *  Group Members:
+ *  Teresita Leonor S. Esver
+ *  Patriz Elaine M. Daroy
+ *  Alexa Tang
+ */
 
-package Process-Creation_Hierarchy;
 import java.util.LinkedList;
 
-public class PCB
+public class PCB_V1
 {
     private int index_parent;
-
     //any processes created by the parent process are
     //held in this linked list of child processes
-    LinkedList<PCB> children = new LinkedList<>();
-
-    PCB(int p_i)
+    LinkedList<PCB_V1> children;
+    //constructor
+    public PCB_V1(int p_i)
     {
-        p_i = index_parent;
+        index_parent = p_i;
+        children = new LinkedList<>();
     }
 
-    public void addChild(PCB child)
+    //add child to linked list of chldren
+    public void addChild(int parentIndex)
     {
-        children.add(child);
+        children.add(new PCB_V1(parentIndex));
     }
-    
-    public LinkedList<PCB> getChildren()
+    //get children of pcb
+    public LinkedList<PCB_V1> getChildren()
     {
         return children;
+    }
+
+    //get youngest child
+    public int getYoungestChildIndex(){
+        return children.size();
+    }
+
+    public int getParentIndex(){
+        return this.index_parent;
     }
 }
