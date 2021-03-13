c9fdd40eb75390713e1cee8a8a814f96aaca29f0
TeraByte1 <teraesver@gmail.com>
2021-03-13 00:58:01 -0800

    update pcb version 2 constructor

--- PCB_V2.java
+++ PCB_V2.java
@@ -7,16 +7,16 @@
 *  Alexa Tang
 */
 
-package Process-Creation_Hierarchy;
+//package Process-Creation_Hierarchy;
 
 public class PCB_V2
 {
     private int parent, first_child, younger_sibling,oldest_sibling;
-    PCB_V2(int p, int f_c, int y_s, int o_s)
+    public PCB_V2(int p, int f_c, int y_s, int o_s)
     {
-        p = parent;
-        f_c = first_child;
-        y_s = younger_sibling;
-        o_s = oldest_sibling;
+        parent=p;
+        first_child=f_c;
+        younger_sibling=y_s;
+        oldest_sibling=o_s;
     }
 }
