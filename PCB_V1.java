a9b42c51ca516c3087a9ec47369259b1a6c5d453
TeraByte1 <teraesver@gmail.com>
2021-03-15 23:52:30 -0700

    update pcb v1

--- PCB_V1.java
+++ PCB_V1.java
@@ -12,8 +12,7 @@ import java.util.LinkedList;
 public class PCB_V1
 {
     private int index_parent;
-    //any processes created by the parent process are
-    //held in this linked list of child processes
+    //any processes created by the parent process are held in this linked list of child processes
     LinkedList<Integer> children;
     //constructor
     public PCB_V1(int p_i)
@@ -21,7 +20,6 @@ public class PCB_V1
         index_parent = p_i;
         children = new LinkedList<>();
     }
-
     //add child to linked list of chldren
     public void addChild(int childIndex)
     {
@@ -32,17 +30,10 @@ public class PCB_V1
     {
         return children;
     }
-
-    //get youngest child
-    public int getYoungestChildIndex(){
-        return children.size();
-    }
-
     public int getParentIndex(){
         return this.index_parent;
     }
-
-    public void removeFromLinkedList() {
-        children.remove(); //removes from head of list
+    public void removeChild(Integer childIndex){
+        children.remove(childIndex);
     }
 }
