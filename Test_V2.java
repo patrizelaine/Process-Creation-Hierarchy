12ba382b438bf4065b01792887bcf1d7d6a327f5
Patriz Elaine Daroy <patrizelaine.daroy@gmail.com>
2021-03-15 21:37:55 -0700

    Refactoring V2 by deleting for-loop

--- Test_V2.java
+++ Test_V2.java
@@ -18,47 +18,41 @@ public class Test_V2 {
 	private static void create(int parentIndex)
 	{
 		int checkChild = pcbArr2[parentIndex].getFirst();
-		for(int i=0; i<pcbArr2.length; i++)
-		{
-			if(i==parentIndex)
+	
+		if(checkChild == -1)	// runs thru this code if the PCB doesn't have any children
+		{			
+			for(int j=0; j<pcbArr2.length; j++)
 			{
-				if(checkChild == -1)	// runs thru this code if the PCB doesn't have any children
+				if(pcbArr2[j]==null)
 				{
-					
-					for(int j=0; j<pcbArr2.length; j++)
-					{
-						if(pcbArr2[j]==null)
-						{
-							pcbArr2[parentIndex].setFirst(j);
-							pcbArr2[j] = new PCB_V2(parentIndex, -1, -1, -1);
-							// System.out.println("Successfully created a child for process " + parentIndex);
-							break;
-						}
+					pcbArr2[parentIndex].setFirst(j);
+					pcbArr2[j] = new PCB_V2(parentIndex, -1, -1, -1);
+					// System.out.println("Successfully created a child for process " + parentIndex);
+					break;
 					}
-				}
-				else if(pcbArr2[checkChild].getOlder() == -1)	// runs thru this code if the PCB only has one child
+			}
+		}
+		else if(pcbArr2[checkChild].getOlder() == -1)	// runs thru this code if the PCB only has one child
+		{
+			for(int j=0; j<pcbArr2.length; j++)
+			{
+				if(pcbArr2[length-1] != null)
 				{
-					for(int j=0; j<pcbArr2.length; j++)
-					{
-						if(pcbArr2[length-1] != null)
-						{
-							length = length*2;
-						}
-						if(pcbArr2[j]==null)
-						{
-							pcbArr2[checkChild].setOlder(j);	// updates the f_c older child
-							pcbArr2[j] = new PCB_V2(parentIndex, -1, checkChild, -1);	// creates a new child
-							// System.out.println("Successfully created a child for process " + parentIndex);
-							break;
-						}
-					}
+					length = length*2;
 				}
-				else if(pcbArr2[checkChild].getOlder() != -1)	// if multiple children
+				if(pcbArr2[j]==null)
 				{
-					int olderChild = pcbArr2[checkChild].getOlder();
-					checkOlderChild(parentIndex, olderChild);
+					pcbArr2[checkChild].setOlder(j);	// updates the f_c older child
+					pcbArr2[j] = new PCB_V2(parentIndex, -1, checkChild, -1);	// creates a new child
+					// System.out.println("Successfully created a child for process " + parentIndex);
+					break;
 				}
 			}
+		}
+		else if(pcbArr2[checkChild].getOlder() != -1)	// if multiple children
+		{
+			int olderChild = pcbArr2[checkChild].getOlder();
+			checkOlderChild(parentIndex, olderChild);
 		}
 	}
 	
