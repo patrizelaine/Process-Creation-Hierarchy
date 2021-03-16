c891c7894603285e3573d0ea6557cfce3e332a9e
Patriz Elaine Daroy <patrizelaine.daroy@gmail.com>
2021-03-15 20:55:26 -0700

    Updating with test cases

--- Test_V2.java
+++ Test_V2.java
@@ -9,7 +9,10 @@ public class Test_V2 {
 	
 	public static void main(String[] args)
 	{
-		runTest(1000);
+		runTest1(1000);
+		runTest2(1000);
+		runTest3(1000);
+		runTest4(1000);
 	}
 	
 	private static void create(int parentIndex)
@@ -21,17 +24,14 @@ public class Test_V2 {
 			{
 				if(checkChild == -1)	// runs thru this code if the PCB doesn't have any children
 				{
+					
 					for(int j=0; j<pcbArr2.length; j++)
 					{
-						if(pcbArr2[length-1] != null)
-						{
-							length = length*2;
-						}
 						if(pcbArr2[j]==null)
 						{
 							pcbArr2[parentIndex].setFirst(j);
 							pcbArr2[j] = new PCB_V2(parentIndex, -1, -1, -1);
-							System.out.println("Successfully created a child for process " + parentIndex);
+							// System.out.println("Successfully created a child for process " + parentIndex);
 							break;
 						}
 					}
@@ -48,7 +48,7 @@ public class Test_V2 {
 						{
 							pcbArr2[checkChild].setOlder(j);	// updates the f_c older child
 							pcbArr2[j] = new PCB_V2(parentIndex, -1, checkChild, -1);	// creates a new child
-							System.out.println("Successfully created a child for process " + parentIndex);
+							// System.out.println("Successfully created a child for process " + parentIndex);
 							break;
 						}
 					}
@@ -73,7 +73,7 @@ public class Test_V2 {
 				{
 					pcbArr2[olderChild].setOlder(j);	// updates the f_c older child
 					pcbArr2[j] = new PCB_V2(parentIndex, -1, olderChild, -1);	// creates a new child
-					System.out.println("Successfully created a child for process " + parentIndex);
+					// System.out.println("Successfully created a child for process " + parentIndex);
 					break;
 				}
 			}
@@ -89,7 +89,7 @@ public class Test_V2 {
 		int firstChild = pcbArr2[destroyIndex].getFirst();
 		if(firstChild != -1)	// if the destroyed index has a first child, then destroy the first child
 		{
-			pcbArr2[destroyIndex].setFirst(pcbArr2[firstChild].getOlder());
+			pcbArr2[destroyIndex].setFirst(pcbArr2[firstChild].getOlder());	// updates the process's parent's first_child to the process's older_child
 	
 			destroy(firstChild);	
 			destroy(destroyIndex);
@@ -97,13 +97,12 @@ public class Test_V2 {
 		else
 		{
 			pcbArr2[destroyIndex] = null;
-			System.out.println("Successfully destroyed process " + destroyIndex);
+			// System.out.println("Successfully destroyed process " + destroyIndex);
 		}
 	}
 	
 	private static void test1()
 	{
-		System.out.println("Executing Test #1");
 		create(0);
 		create(0);
 		create(2);
@@ -111,18 +110,91 @@ public class Test_V2 {
 		destroy(0);
 	}
 	
-	private static void runTest(int iterations)
+	private static void test2()
+	{
+		create(0);
+		create(0);
+		create(2);
+		create(2);
+		create(2);
+		create(3);
+	}
+	
+	private static void test3()
+	{
+		create(0);
+		create(1);
+		create(2);
+		create(3);
+		destroy(2);
+	}
+	
+	private static void test4()
+	{
+		create(0);
+		create(1);
+		create(1);
+		create(1);
+		destroy(0);
+	}
+	
+	private static void runTest1(int iterations)
 	{
 		createResultsFile(1);
 		for(int i=0; i<iterations; i++)
 		{
 			PCB_V2 pcb0 = new PCB_V2(-1, -1, -1, -1);	// creating first PCB
 			pcbArr2[0] = pcb0;
-			long start1 = System.nanoTime();
+			long start = System.nanoTime();
 			test1();
-			long end1 = System.nanoTime();
-			long runtime1 = end1-start1;
-			writeResultsToFile(runtime1);
+			long end = System.nanoTime();
+			long runtime = end-start;
+			writeResultsToFile(runtime);
+		}
+	}
+	
+	private static void runTest2(int iterations)
+	{
+		createResultsFile(2);
+		for(int i=0; i<iterations; i++)
+		{
+			PCB_V2 pcb0 = new PCB_V2(-1, -1, -1, -1);	// creating first PCB
+			pcbArr2[0] = pcb0;
+			long start = System.nanoTime();
+			test2();
+			long end = System.nanoTime();
+			long runtime = end-start;
+			writeResultsToFile(runtime);
+		}
+	}
+	
+	private static void runTest3(int iterations)
+	{
+		createResultsFile(3);
+		for(int i=0; i<iterations; i++)
+		{
+			PCB_V2 pcb0 = new PCB_V2(-1, -1, -1, -1);	// creating first PCB
+			pcbArr2[0] = pcb0;
+			long start = System.nanoTime();
+			test3();
+			long end = System.nanoTime();
+			long runtime = end-start;
+			writeResultsToFile(runtime);
+		}
+	}
+	
+	private static void runTest4(int iterations)
+	{
+		createResultsFile(4);
+		for(int i=0; i<iterations; i++)
+		{
+			PCB_V2 pcb0 = new PCB_V2(-1, -1, -1, -1);	// creating first PCB
+			pcbArr2[0] = pcb0;
+			long start = System.nanoTime();
+			test4();
+			long end = System.nanoTime();
+			long runtime = end-start;
+			writeResultsToFile(runtime);
 		}
 	}
 	
